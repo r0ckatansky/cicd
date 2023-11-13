4764c4faa42d1424fedc0878283dcbf0b08d42e1
paul <trochel.paul@outlook.fr>
2023-11-13 18:15:44 +0100

    C'EST BON OMG ?

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,35 @@
-const assert = require('chai').assert;
+//describe('Simple Test', () => {
+//  it('should pass', () => {
+//    assert.equal(1, 1);
+//  });
+//});
 
-describe('Simple Test', () => {
-  it('should pass', () => {
-    assert.equal(1, 1);
-  });
-});
+// tests/api.test.js
+const assert = require('chai').assert;
+const request = require('supertest');
+const app = require('../server'); // Assurez-vous que le chemin est correct
+
+class APITestSuite {
+  constructor() {
+    this.app = app;
+  }
+
+  run() {
+    describe('API', () => {
+      it('should return "Hello, World!" when GET /', (done) => {
+        request(this.app)
+          .get('/')
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+            assert.equal(res.text, 'Hello, World!');
+            done();
+          });
+      });
+    });
+  }
+}
+
+const apiTestSuite = new APITestSuite();
+apiTestSuite.run();
 

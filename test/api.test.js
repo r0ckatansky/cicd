b98ab0241f366e898f240deb1dc79aa6ee2c2618
paul <trochel.paul@outlook.fr>
2023-11-14 08:45:52 +0100

    changement du test

--- test/api.test.js
+++ test/api.test.js
@@ -6,37 +6,59 @@
 // });
 
 // tests/api.test.js
+// const assert = require('chai').assert;
+// const request = require('supertest');
+// const app = require('../server'); // Assurez-vous que le chemin est correct
+
+// class APITestSuite {
+//   constructor() {
+//     this.app = app;
+//   }
+
+//   run() {
+//     describe('API', () => {
+//       it('should return "Hello, World!" when GET /', (done) => {
+//         request(this.app)
+//         .get('/')
+//         .expect(200)
+//         .end((err, res) => {
+//           if (err) {
+//             done(err); // Assurez-vous que done est appelé avec l'erreur
+//             return;
+//           }
+//           assert.equal(res.text, 'Hello, World!');
+//           done();
+//         });
+//       });
+//     });
+//   }
+// }
+
+
+// const apiTestSuite = new APITestSuite();
+// apiTestSuite.run();
+
+
+
 const assert = require('chai').assert;
 const request = require('supertest');
 const app = require('../server'); // Assurez-vous que le chemin est correct
-
-class APITestSuite {
-  constructor() {
-    this.app = app;
-  }
-
-  run() {
-    describe('API', () => {
-      it('should return "Hello, World!" when GET /', (done) => {
-        request(this.app)
-        .get('/')
-        .expect(200)
-        .end((err, res) => {
-          if (err) {
-            done(err); // Assurez-vous que done est appelé avec l'erreur
-            return;
-          }
-          assert.equal(res.text, 'Hello, World!');
-          done();
-        });
-      });
+describe('API', () => {
+  it('should return "Hello, World!" when GET /', (done) => {
+    request(app)
+    .get('/')
+    .expect(200)
+    .end((err, res) => {
+      if (err) {
+        done(err); // Assurez-vous que done est appelé avec l'erreur
+        return;
+      }
+      assert.equal(res.text, 'Hello, World!');
+      done();
     });
-  }
-}
-
-
+  });
+});
 
 
 const apiTestSuite = new APITestSuite();
 apiTestSuite.run();
-

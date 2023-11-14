13a60adcc2c0abd498f18249140630508c57ce23
paul <trochel.paul@outlook.fr>
2023-11-14 08:30:06 +0100

    changement des dépendances (remove de package-lock.json)

--- test/api.test.js
+++ test/api.test.js
@@ -1,36 +1,36 @@
-const assert = require('chai').assert;
-describe('Simple Test', () => {
- it('should pass', () => {
-   assert.equal(1, 1);
- });
-});
+// const assert = require('chai').assert;
+// describe('Simple Test', () => {
+//  it('should pass', () => {
+//    assert.equal(1, 1);
+//  });
+// });
 
 // tests/api.test.js
-// const assert = require('chai').assert;
-// const request = require('supertest');
-// const app = require('../server'); // Assurez-vous que le chemin est correct
+const assert = require('chai').assert;
+const request = require('supertest');
+const app = require('../server'); // Assurez-vous que le chemin est correct
 
-// class APITestSuite {
-//   constructor() {
-//     this.app = app;
-//   }
+class APITestSuite {
+  constructor() {
+    this.app = app;
+  }
 
-//   run() {
-//     describe('API', () => {
-//       it('should return "Hello, World!" when GET /', (done) => {
-//         request(this.app)
-//           .get('/')
-//           .expect(200)
-//           .end((err, res) => {
-//             if (err) return done(err);
-//             assert.equal(res.text, 'Hello, World!');
-//             done();
-//           });
-//       });
-//     });
-//   }
-// }
+  run() {
+    describe('API', () => {
+      it('should return "Hello, World!" when GET /', (done) => {
+        request(this.app)
+          .get('/')
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done(err);
+            assert.equal(res.text, 'Hello, World!');
+            done();
+          });
+      });
+    });
+  }
+}
 
-// const apiTestSuite = new APITestSuite();
-// apiTestSuite.run();
+const apiTestSuite = new APITestSuite();
+apiTestSuite.run();
 
